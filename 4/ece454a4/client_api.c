a94532647bc6924b9533640c86ea8e38a9523075
Sola Ogunsakin <juniorsola@yahoo.com>
2014-07-19 22:30:09 -0400

    sleep only 3 secs

--- 4/ece454a4/client_api.c
+++ 4/ece454a4/client_api.c
@@ -229,7 +229,7 @@ int fsOpen(const char *fname, int mode) {
     int attempts = 0, retval;
     fs_response *response;
     do {
-        if (attempts++ > 0) sleep(30);
+        if (attempts++ > 0) sleep(3);
         ans = make_remote_call(info->ip_or_domain,
                                info->port_no, "fsOpen", 2,
                                strlen(path) + 1, (void *) path,
